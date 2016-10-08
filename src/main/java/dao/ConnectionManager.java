2106f40fc4c30e702a50f8110b8d7bfb02a9f4de
vladgnes <Vhnes02@gmail.com>
2016-10-09 01:11:51 +0300

    Add ConnectionManager() method.

---
+++ src/main/java/dao/ConnectionManager.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author vlad
+ */
+import static dao.DAO.databaseName;
+ import java.sql.*;
+
+
+   public class ConnectionManager {
+
+      static Connection con;
+      static String url;
+            
+      public static Connection getConnection(String userName, String password, String databaseName)
+      {
+        
+         try
+         {
+            String url = "jdbc:mysql://localhost:3306/"+ databaseName; 
+            Class.forName("com.mysql.jdbc.Driver");
+            try
+            {            	
+               con = DriverManager.getConnection(url,userName,password); 
+                								
+                 
+            }
+            
+            catch (SQLException ex)
+            {
+               ex.printStackTrace();
+            }
+         }
+
+         catch(ClassNotFoundException e)
+         {
+            System.out.println(e);
+         }
+
+      return con;
+      }
+    }
