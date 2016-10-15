7d8a37e36c054c326c15721266a728cc7e00ac0b
vladgnes <https://github.com/vladgnes>
2016-10-15 03:36:58 +0300

    Add get and set methods for subject variable.

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -4,4 +4,13 @@ package model;
  * Created by vlad on 27.08.2016.
  */
 public class Teacher extends User{
+    private String subject;
+    
+     public void setSubject(String subject){
+        this.subject = subject;
+    }
+
+    public String getSubject(){
+        return subject;
+    }
 }
