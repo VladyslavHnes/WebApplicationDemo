6e69b213866f578f60bbbb04385f6a26b683f0a6
vladgnes <https://github.com/vladgnes>
2016-10-15 03:37:38 +0300

    Write DAOLogin class.

---
+++ src/main/java/dao/DAOLogin.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Student;
+import model.Teacher;
+import model.User;
+
+/**
+ *
+ * @author Admin
+ */
+public class DAOLogin{
+    final static String studentRequest = "SELECT ID FROM students WHERE login = ? and password = ?";
+    final static String teacherRequest = "SELECT ID FROM teachers WHERE login = ? and password = ?";
+    static ResultSet studentResSet;
+    static ResultSet teacherResSet;
+    
+    
+    //Check if there is a student with current login and password and return object
+    //If there is no the student, then return null
+    public static Student studentRequest(String login, String password) throws SQLException{
+        PreparedStatement preparedStatement = ConnectionManager.con.prepareStatement(studentRequest);
+        preparedStatement.setString(1, login);
+        preparedStatement.setString(2, password);
+        if(preparedStatement.executeQuery()!= null){
+            return DAOLogin.getStudentObject(preparedStatement.executeQuery());
+        }else{
+            return null;
+        }
+    }
+    
+    //Get student object from result set that we generate when we send student request
+    public static Student getStudentObject(ResultSet resultSet)throws SQLException{
+        Student student = new Student();
+        student.setFirstName(resultSet.getString("firstName"));
+        student.setLastName(resultSet.getString("lastName"));
+        student.setLogin(resultSet.getString("login"));
+        student.setFirstName(resultSet.getString("password"));
+        return student;
+    }
+    
+    //Check if there is a teacher with current login and password and return object
+    //If there is no the teacher, then return null
+    public static Teacher teacherRequest(String login, String password) throws SQLException{
+        PreparedStatement preparedStatement = ConnectionManager.con.prepareStatement(teacherRequest);
+        preparedStatement.setString(1, login);
+        preparedStatement.setString(2, password);
+        if(preparedStatement.executeQuery()!= null){
+            return DAOLogin.getTeacherObject(preparedStatement.executeQuery());
+        }else{
+            return null;
+        }
+    }
+    
+    //Get teacher object from result set that we generate when we send teacher request
+    public static Teacher getTeacherObject(ResultSet resultSet)throws SQLException{
+        Teacher teacher = new Teacher();
+        teacher.setFirstName(resultSet.getString("firstName"));
+        teacher.setLastName(resultSet.getString("lastName"));
+        teacher.setLogin(resultSet.getString("login"));
+        teacher.setFirstName(resultSet.getString("password"));
+        teacher.setSubject(resultSet.getString("subject"));
+        return teacher;
+    }
+}
