36fcabcdf8a80129464e0eb189b96d387432c9a5
vladgnes <Vhnes02@gmail.com>
2016-10-17 05:07:27 +0300

    Create DAORegistry method and DAORegistery test.

---
+++ src/main/java/dao/DAORegistry.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author vlad
+ */
+public class DAORegistry {
+    
+    final static String StudentRegRequest = "INSERT INTO students(lastName,firstName,login,password) VALUES(?,?,?,?)";
+    final static String StudentSelectRequest = "SELECT login FROM students WHERE login = ?";
+    
+    public static boolean regStudent(String lastName,String firstName,String login, String password) throws SQLException{
+        Connection connection = DAOConnectionManager.getConnection();
+        if(!DAORegistry.ifUserExist(login, password)){
+            PreparedStatement preparedStatement = connection.prepareStatement(StudentRegRequest);
+            preparedStatement.setString(1,lastName);
+            preparedStatement.setString(2,firstName);
+            preparedStatement.setString(3,login);
+            preparedStatement.setString(4,password);
+            preparedStatement.executeUpdate();
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public static boolean ifUserExist(String login,String password) throws SQLException{
+        Connection connection = DAOConnectionManager.getConnection();
+        PreparedStatement preparedSelectStatement = connection.prepareStatement(StudentSelectRequest);
+        preparedSelectStatement.setString(1, login);
+        ResultSet queryResult = preparedSelectStatement.executeQuery();
+        return queryResult.next();
+    }
+}
---
+++ src/test/java/dao/DAORegistryTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author vlad
+ */
+public class DAORegistryTest {
+    
+    
+
+    /**
+     * Test of regStudent method, of class DAORegistry.
+     */
+    @Test
+    public void testRegStudent() throws Exception {
+        System.out.println("regStudent");
+        String lastName = "las";
+        String firstName = "sad";
+        String login = "Sossad";
+        String password = "Pisos";
+        boolean expResult = true;
+        boolean result = DAORegistry.regStudent(lastName, firstName, login, password);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of ifUserExist method, of class DAORegistry.
+     */
+    @Test
+    public void testIfUserExist() throws Exception {
+        System.out.println("ifUserExist");
+        String login = "Sos";
+        String password = "Pisos123";
+        boolean expResult = true;
+        boolean result = DAORegistry.ifUserExist(login, password);
+        assertEquals(expResult, result);
+    }
+    
+}
