5efb0aa0a644e45e2e567c9dec9572c184908c8f
vladgnes <Vhnes02@gmail.com>
2016-10-17 05:06:10 +0300

    Small fix.

--- src/test/java/dao/DAOLoginTest.java
+++ src/test/java/dao/DAOLoginTest.java
@@ -20,25 +20,6 @@ import static org.junit.Assert.*;
  */
 public class DAOLoginTest {
     
-    public DAOLoginTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of studentRequest method, of class DAOLogin.
      */
