cf5eb9f73a9df812b1c3de7befde59c06bca6163
vladgnes <Vhnes02@gmail.com>
2016-10-18 23:44:18 +0300

    Delete unnecessary imports.

--- src/test/java/dao/DAORegistryTest.java
+++ src/test/java/dao/DAORegistryTest.java
@@ -5,11 +5,6 @@
  */
 package dao;
 
-import java.sql.Connection;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
