0258877a4c75c4df9da29ff57c1e74519ee9f7fb
Unknown <Vhnes02@gmail.com>
2016-10-27 02:46:29 +0300

    Write DAOSubscribe class.

---
+++ src/test/java/dao/DAOSubscribeTest.java
@@ -0,0 +1,25 @@
+package dao;
+
+import dao.requests.DAOSubscribe;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vlad on 26.10.16.
+ */
+public class DAOSubscribeTest {
+
+    @Test
+    public void testSubscribe() throws SQLException {
+        System.out.println("subscribe");
+        String subject = "Java";
+        String lastName = "Ruslan";
+        String firstName = "Vdovichenko";
+        int expResult = 1;
+        int result = DAOSubscribe.subscribe(subject,lastName,firstName);
+        assertEquals(expResult, result);
+    }
+}
