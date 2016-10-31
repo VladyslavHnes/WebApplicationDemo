cb109c507f4c07ce948db0b5ea1b9fda39456b50
Unknown <Vhnes02@gmail.com>
2016-10-31 20:57:40 +0200

    Create Course class.

--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -4,4 +4,33 @@ package model;
  * Created by vlad on 27.10.16.
  */
 public class Course {
+
+    private String firstNameOfTeacher;
+    private String lastNameOfTeacher;
+    private String subject;
+
+
+    public String getFirstNameOfTeacher() {
+        return firstNameOfTeacher;
+    }
+
+    public void setFirstNameOfTeacher(String firstNameOfTeacher) {
+        this.firstNameOfTeacher = firstNameOfTeacher;
+    }
+
+    public String getLastNameOfTeacher() {
+        return lastNameOfTeacher;
+    }
+
+    public void setLastNameOfTeacher(String lastNameOfTeacher) {
+        this.lastNameOfTeacher = lastNameOfTeacher;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
