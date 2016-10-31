a4df5cdc9617275672d4063b8e1a219cda69d89f
Unknown <Vhnes02@gmail.com>
2016-10-31 20:56:16 +0200

    Create DAOShowCourses.

--- src/main/java/dao/requests/DAOShowCourses.java
+++ src/main/java/dao/requests/DAOShowCourses.java
@@ -1,10 +1,39 @@
 package dao.requests;
 
 import dao.DAOInterface;
+import model.Course;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by vlad on 26.10.16.
  */
 public class DAOShowCourses implements DAOInterface {
 
+    public static ArrayList<Course> arrayList;
+
+    public static ArrayList<Course> getCourses()throws Exception{
+        URL url = DAOShowCourses.class.getResource("database/Courses");
+        File file = new File(url.getPath());
+        BufferedReader brCourses = new BufferedReader(new FileReader(file));
+        BufferedReader brTeacher = new BufferedReader(new FileReader("database/Teachers"));
+        String lineCourses;
+        String lineTeachers;
+        while(((lineCourses = brCourses.readLine()) != null) &&
+                (lineTeachers = brTeacher.readLine()) != null) {
+            Course course = new Course();
+            course.setSubject(lineCourses);
+            String[] arrayTeacher = lineTeachers.split(" ");
+            course.setFirstNameOfTeacher(arrayTeacher[0]);
+            course.setLastNameOfTeacher(arrayTeacher[1]);
+            arrayList.add(course);
+        }
+        return arrayList;
+
+    }
+
+
 }
+
