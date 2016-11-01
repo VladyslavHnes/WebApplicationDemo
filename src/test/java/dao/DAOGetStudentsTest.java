6239d804d18d5cacec88143bf40abb0790177df0
Unknown <Vhnes02@gmail.com>
2016-11-01 23:49:38 +0200

    Write test for DAOGetStudents.

---
+++ src/test/java/dao/DAOGetStudentsTest.java
@@ -0,0 +1,26 @@
+package dao;
+
+import dao.requests.DAOGetMark;
+import dao.requests.DAOGetStudents;
+import model.Student;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by vlad on 01.11.16.
+ */
+public class DAOGetStudentsTest implements DAOInterface {
+    @Test
+    public void getStudentsTest() throws SQLException {
+        String firstName = "Vlad";
+        String lastName = "Hnes";
+        Student student = new Student();
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        assertEquals(student.getFirstName(), DAOGetStudents.getStudents("Java").get(0).getFirstName());
+        assertEquals(student.getLastName(), DAOGetStudents.getStudents("Java").get(0).getLastName());
+    }
+}
