30f50a6c59b09d9d90c38ed0bd18114f502d13f2
Unknown <Vhnes02@gmail.com>
2016-11-01 04:38:55 +0200

    Write SignUpStudentController class.

--- src/main/java/controller/student/SignUpStudentController.java
+++ src/main/java/controller/student/SignUpStudentController.java
@@ -1,9 +1,17 @@
 package controller.student;
 
+import dao.requests.DAOLogin;
+import dao.requests.DAORegistry;
+import dao.requests.DAOShowCourses;
+import model.Course;
+import model.Student;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 /**
  * Created by vlad on 26.10.16.
@@ -11,5 +19,30 @@ import javax.servlet.http.HttpServletResponse;
 public class SignUpStudentController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, java.io.IOException {
+        try {
+            String login = request.getParameter("login");
+            String password = request.getParameter("password");
+            String firstName = request.getParameter("firstname");
+            String lastName = request.getParameter("lastname");
+            Student student = new Student();
+            student.setLogin(login);
+            student.setPassword(password);
+            student.setFirstName(firstName);
+            student.setLastName(lastName);
+            ArrayList<Course> courses = DAOShowCourses.getCourses();
+            HttpSession session = request.getSession(true);
+            session.setAttribute("courses",courses);
+            boolean flag = DAORegistry.regStudent(student.getFirstName(),student.getLastName(),
+                    student.getLogin(),student.getPassword());
+            if (flag) {
+                session.setAttribute("student",student);
+                response.sendRedirect("SubscribeCoursePage.jsp"); //logged-in page
+            }
+            else
+                response.sendRedirect("ErrorPage.jsp"); //error page
+        }
+        catch (Throwable theException) {
+            System.out.println(theException);
+        }
     }
 }
