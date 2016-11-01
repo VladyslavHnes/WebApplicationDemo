f94b5917a27ee32ee1cc6d191c4776b9476a71a8
Unknown <Vhnes02@gmail.com>
2016-11-01 04:41:01 +0200

    Changes in DAOShowCourses.

--- src/main/java/dao/requests/DAOShowCourses.java
+++ src/main/java/dao/requests/DAOShowCourses.java
@@ -2,6 +2,7 @@ package dao.requests;
 
 import dao.DAOInterface;
 import model.Course;
+import model.Teacher;
 
 import java.io.*;
 import java.net.URL;
@@ -12,22 +13,25 @@ import java.util.ArrayList;
  */
 public class DAOShowCourses implements DAOInterface {
 
-    public static ArrayList<Course> arrayList;
-
-    public static ArrayList<Course> getCourses()throws Exception{
-        URL url = DAOShowCourses.class.getResource("database/Courses");
-        File file = new File(url.getPath());
-        BufferedReader brCourses = new BufferedReader(new FileReader(file));
-        BufferedReader brTeacher = new BufferedReader(new FileReader("database/Teachers"));
+    public static ArrayList<Course> getCourses() throws java.io.IOException{
+        ArrayList<Course> arrayList = new ArrayList<Course>();
+        Class cl = DAOShowCourses.class;
+        ClassLoader classLoader = cl.getClassLoader();
+        URL urlCourses = classLoader.getResource("database/Courses");
+        File fileCourses = new File(urlCourses.getPath());
+        URL urlTeachers = classLoader.getResource("database/Teachers");
+        File fileTeachers = new File(urlTeachers.getPath());
+        BufferedReader brCourses = new BufferedReader(new FileReader(fileCourses));
+        BufferedReader brTeacher = new BufferedReader(new FileReader(fileTeachers));
         String lineCourses;
         String lineTeachers;
         while(((lineCourses = brCourses.readLine()) != null) &&
                 (lineTeachers = brTeacher.readLine()) != null) {
             Course course = new Course();
             course.setSubject(lineCourses);
-            String[] arrayTeacher = lineTeachers.split(" ");
-            course.setFirstNameOfTeacher(arrayTeacher[0]);
-            course.setLastNameOfTeacher(arrayTeacher[1]);
+            String [] teacherLastNameAndFirstName = lineTeachers.split(" ");
+            course.setFirstNameOfTeacher(teacherLastNameAndFirstName[0]);
+            course.setLastNameOfTeacher(teacherLastNameAndFirstName[1]);
             arrayList.add(course);
         }
         return arrayList;
