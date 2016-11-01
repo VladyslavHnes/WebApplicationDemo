020ed19841172dbee00a8c91bfb670cab1a1e929
Unknown <Vhnes02@gmail.com>
2016-11-01 18:00:54 +0200

    Use DAOShowCourses to set marked subjects for current session user.

--- src/main/java/controller/student/SubscribeStudentController.java
+++ src/main/java/controller/student/SubscribeStudentController.java
@@ -1,14 +1,14 @@
 package controller.student;
 
-import dao.requests.DAOShowCourses;
-import model.Course;
+import dao.requests.DAOSetSubject;
+import model.Student;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
+import java.sql.SQLException;
 
 /**
  * Created by vlad on 26.10.16.
@@ -23,10 +23,17 @@ public class SubscribeStudentController extends HttpServlet {
             response.sendRedirect("SubscribeCoursePage.jsp");
         }
         else {
-            for (String s: subscribedCourses)
-                System.out.println(s);
+            Student student = (Student) session.getAttribute("student");
+            String firstName = student.getFirstName();
+            String lastName = student.getLastName();
+            for (String subject: subscribedCourses) {
+                try {
+                    DAOSetSubject.setSubject(subject, firstName, lastName);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
 
     }
 
