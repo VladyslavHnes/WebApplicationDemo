1d31c5b14d30df1eecc718a430defd90a927986e
Unknown <Vhnes02@gmail.com>
2016-11-01 17:59:28 +0200

    Write test for DAOSetSubject class.

---
+++ src/test/java/dao/DAOSetSubjectTest.java
@@ -0,0 +1,23 @@
+package dao;
+
+import dao.requests.DAOSetMark;
+import dao.requests.DAOSetSubject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by vlad on 01.11.16.
+ */
+public class DAOSetSubjectTest {
+    @Test
+    public void testSetSubject() throws Exception {
+        System.out.println("setSubject");
+        String subject = "Java";
+        String lastName = "Ruslan";
+        String firstName = "Vdovichenko";
+        int expResult = 1;
+        int result = DAOSetSubject.setSubject(subject,firstName,lastName);
+        assertEquals(expResult, result);
+    }
+}
