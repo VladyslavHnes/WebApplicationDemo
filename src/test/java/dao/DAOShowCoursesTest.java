3ee72e03c8d149209400c41ade0772f428499629
Unknown <Vhnes02@gmail.com>
2016-11-01 04:38:04 +0200

    Delete useless fragment of code.

--- src/test/java/dao/DAOShowCoursesTest.java
+++ src/test/java/dao/DAOShowCoursesTest.java
@@ -13,23 +13,13 @@ import static org.junit.Assert.*;
 public class DAOShowCoursesTest {
 
     @Test
-    public void showCoursesTest() throws Exception{
+    public void showCoursesTest() throws Exception {
         Course java = new Course();
         java.setSubject("Java");
         java.setFirstNameOfTeacher("James");
         java.setLastNameOfTeacher("Gosling");
-        Course javaScript = new Course();
-        java.setSubject("JavaScript");
-        java.setFirstNameOfTeacher("Brendan");
-        java.setLastNameOfTeacher("Eich");
-        Course dataStructures = new Course();
-        java.setSubject("DataStructures");
-        java.setFirstNameOfTeacher("Thomas");
-        java.setLastNameOfTeacher("Cormen");
         assertEquals(java.getSubject(), DAOShowCourses.getCourses().get(0).getSubject());
         assertEquals(java.getFirstNameOfTeacher(), DAOShowCourses.getCourses().get(0).getFirstNameOfTeacher());
         assertEquals(java.getLastNameOfTeacher(), DAOShowCourses.getCourses().get(0).getLastNameOfTeacher());
     }
-
-
 }
