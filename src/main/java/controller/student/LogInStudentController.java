01fb61fa71aa74f4dff8c516cbe278a33fc6fecf
Unknown <Vhnes02@gmail.com>
2016-11-01 20:10:31 +0200

    Rewrite LogInStudentController.

--- src/main/java/controller/student/LogInStudentController.java
+++ src/main/java/controller/student/LogInStudentController.java
@@ -5,8 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.requests.DAOGetMark;
 import dao.requests.DAOLogin;
+import dao.requests.DAOShowCourses;
+import model.Course;
 import model.Student;
+
+import java.util.ArrayList;
 
 /**
  * Created by vlad on 19.09.2016.
@@ -14,15 +19,27 @@ import model.Student;
 public class LogInStudentController extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, java.io.IOException {
+            throws ServletException, java.io.IOException, NullPointerException {
         try {
             String login = request.getParameter("login");
             String password = request.getParameter("password");
             Student student = DAOLogin.studentRequest(login, password);
+            String firstName = student.getFirstName();
+            String lastName = student.getLastName();
+            ArrayList<Course> courses = DAOShowCourses.getCourses();
+            HttpSession session = request.getSession(true);
+            session.setAttribute("courses",courses);
+            ArrayList<Integer> marks = new ArrayList<>();
+            int javaMark = DAOGetMark.getMark("Java",firstName,lastName);
+            marks.add(javaMark);
+            int javaScriptMark = DAOGetMark.getMark("JavaScript",firstName,lastName);
+            marks.add(javaScriptMark);
+            int dataStructuresMark = DAOGetMark.getMark("DataStructures",firstName,lastName);
+            marks.add(dataStructuresMark);
             if (student != null) {
-                HttpSession session = request.getSession(true);
-                session.setAttribute("currentSessionUser",student); 
-                response.sendRedirect("ViewCoursesPage.jsp"); //logged-in page
+                session.setAttribute("marks",marks);
+                session.setAttribute("student",student);
+                response.sendRedirect("StudentProfilePage.jsp"); //logged-in page
             }
             else
                 response.sendRedirect("ErrorPage.jsp"); //error page
