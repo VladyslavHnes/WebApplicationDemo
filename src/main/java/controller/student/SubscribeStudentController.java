35188975d3a745c686dc96a8a59d4f546bb9603a
Unknown <Vhnes02@gmail.com>
2016-11-03 22:35:22 +0200

    Write subscribe student controller.

--- src/main/java/controller/student/SubscribeStudentController.java
+++ src/main/java/controller/student/SubscribeStudentController.java
@@ -1,6 +1,10 @@
 package controller.student;
 
+import dao.requests.DAOGetMark;
+import dao.requests.DAOLogin;
 import dao.requests.DAOSetSubject;
+import dao.requests.DAOShowCourses;
+import model.Course;
 import model.Student;
 
 import javax.servlet.ServletException;
@@ -9,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Created by vlad on 26.10.16.
@@ -26,12 +31,33 @@ public class SubscribeStudentController extends HttpServlet {
             Student student = (Student) session.getAttribute("student");
             String firstName = student.getFirstName();
             String lastName = student.getLastName();
+            ArrayList<Course> courses = DAOShowCourses.getCourses();
+            session.setAttribute("courses",courses);
+            try {
+                student = DAOLogin.studentRequest((String)session.getAttribute("login"),
+                        (String)session.getAttribute("password"));
+                ArrayList<Integer> marks = new ArrayList<>();
+                int javaMark = DAOGetMark.getMark("Java",firstName,lastName);
+                marks.add(javaMark);
+                int javaScriptMark = DAOGetMark.getMark("JavaScript",firstName,lastName);
+                marks.add(javaScriptMark);
+                int dataStructuresMark = DAOGetMark.getMark("DataStructures",firstName,lastName);
+                marks.add(dataStructuresMark);
+                session.setAttribute("marks",marks);
+                session.setAttribute("student",student);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
             for (String subject: subscribedCourses) {
                 try {
                     DAOSetSubject.setSubject(subject, firstName, lastName);
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
+            }
+            if(!response.isCommitted()) {
+                response.sendRedirect("StudentProfilePage.jsp");
             }
         }
 
