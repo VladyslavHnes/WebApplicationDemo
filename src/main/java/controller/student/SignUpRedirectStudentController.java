e0538f5e4e5a51d08723f0d090577e7a5adb53f8
Unknown <Vhnes02@gmail.com>
2016-11-03 22:36:44 +0200

    Rename.

---
+++ src/main/java/controller/student/SignUpRedirectStudentController.java
@@ -0,0 +1,22 @@
+package controller.student;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by vlad on 26.10.16.
+ */
+public class SignUpRedirectStudentController extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, java.io.IOException {
+        try {
+            response.sendRedirect("SignUpStudentPage.jsp"); //signed-up page
+        }
+        catch (Throwable theException) {
+            System.out.println(theException);
+        }
+    }
+}
