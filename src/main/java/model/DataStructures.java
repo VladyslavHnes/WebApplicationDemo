64dea12fe99ae65518466255a51793efc174feb2
vladgnes <Vhnes02@gmail.com>
2017-01-11 13:48:33 +0200

    Create class for DataStructures class.

---
+++ src/main/java/model/DataStructures.java
@@ -0,0 +1,12 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by vlad on 10.01.17.
+ */
+@Entity
+@Table(name = "DataStructures")
+public class DataStructures extends Course {
+}
