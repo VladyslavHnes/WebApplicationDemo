6210d0b819e69391f37c5e04851b27e8263e3e91
vladgnes <Vhnes02@gmail.com>
2017-01-28 21:28:37 +0200

    Change tests methods, because of  making DAOHibernateStudent methods static

--- src/test/java/dao/hibernate/DAOHibernateStudentTest.java
+++ src/test/java/dao/hibernate/DAOHibernateStudentTest.java
@@ -11,27 +11,27 @@ public class DAOHibernateStudentTest {
 
     @Test
     public void testDAOHibernateGetStudentsTest(){
-        assertEquals("Hnes",new DAOHibernateStudent().getAll().get(0).getLastName());
+        assertEquals("Hnes", DAOHibernateStudent.getAll().get(0).getLastName());
     }
 
     @Test
     public void testDAOHibernateLoginStudentTest(){
-        assertEquals("Hnes",new DAOHibernateStudent().login("Vlad","Hnes123").getLastName());
+        assertEquals("Hnes", DAOHibernateStudent.login("Vlad","nes123").getLastName());
     }
 
     @Test
     public void testDAOHibernateIfUserExists(){
-        assertEquals(true, new DAOHibernateStudent().ifUserExists("Vlad","Hnes123"));
+        assertEquals(true, DAOHibernateStudent.ifUserExists("Vlad","Hnes123"));
     }
 
     @Test
     public void testDAOHibernateRegistry(){
-        assertEquals(true, new DAOHibernateStudent().registry("Chris","Pratt","Chris","Pratt123"));
+        assertEquals(true, DAOHibernateStudent.registry("Chris","Pratt","Chris","Pratt123"));
     }
 
     @Test
     public void testDAOHibernateGetMark(){
-        assertEquals(4, new DAOHibernateStudent().getMark("JavaScript","Vlad","Hnes"));
+        assertEquals(4, DAOHibernateStudent.getMark("JavaScript","Vlad","Hnes"));
     }
 
 }
